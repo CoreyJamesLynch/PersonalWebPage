8b5382fd536a56f185c02dddc0aa75ccadc5b064
bearinawolfpack <bearinawolfpack@gmail.com>
2021-01-27 10:51:55 -0500

    Install initial dependencies
    Bootstrap
    React-Bootstrap
    React-Dom-Router
    Prettier Config
    ESLinter Config
    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Welcome to the start of your site Corey!</h1>
+    </>
+  );
 }
 
 export default App;
